0c70845c1a2641015f5f9a774f7d51c002516610
khack_j0k3R <khack.joker@gmail.com>
2019-03-15 18:48:22 +0700

    CK-93: [Desktop] Remove unnecessary notices in direct/room chat

--- src/components/views/rooms/EventTile.js
+++ src/components/views/rooms/EventTile.js
@@ -523,6 +523,11 @@ module.exports = withMatrixClient(React.createClass({
         const isSending = (['sending', 'queued', 'encrypting'].indexOf(this.props.eventSendStatus) !== -1);
         const isRedacted = isMessageEvent(this.props.mxEvent) && this.props.isRedacted;
         const isEncryptionFailure = this.props.mxEvent.isDecryptionFailure();
+        const isRedundant = isRedundantEventMessage(this.props.mxEvent);
+
+        if (isRedundant || isEncryptionFailure) {
+            return (<div></div>);
+        }
 
         const classes = classNames({
             mx_EventTile: true,
@@ -736,9 +741,7 @@ module.exports = withMatrixClient(React.createClass({
                     this.props.permalinkCreator,
                     'replyThread',
                 );
-                if (isEncryptionFailure) {
-                    return (<div></div>);
-                }
+
                 return (
                     <div className={classes}>
                         <div className="mx_EventTile_msgOption">
@@ -777,6 +780,11 @@ module.exports = withMatrixClient(React.createClass({
 const messageTypes = ['m.room.message', 'm.sticker'];
 function isMessageEvent(ev) {
     return (messageTypes.includes(ev.getType()));
+}
+
+const redundantType = ['m.room.join_rules', 'm.room.history_visibility', 'm.room.guest_access', 'm.room.encryption'];
+function isRedundantEventMessage(ev) {
+    return (redundantType.includes(ev.getType()));
 }
 
 module.exports.haveTileForEvent = function(e) {
