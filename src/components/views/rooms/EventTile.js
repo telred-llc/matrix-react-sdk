639b88b044bfcd8aa251c86e8583657726b7e824
Ha Nguyen <mjboy1412@gmail.com>
2019-03-18 23:25:50 +0700

    CK-100: remove the warning icon

--- src/components/views/rooms/EventTile.js
+++ src/components/views/rooms/EventTile.js
@@ -722,7 +722,7 @@ module.exports = withMatrixClient(React.createClass({
                             <a href={permalink} onClick={this.onPermalinkClicked}>
                                 { timestamp }
                             </a>
-                            { this._renderE2EPadlock() }
+                            {/*{ this._renderE2EPadlock() }*/}
                             { thread }
                             <EventTileType ref="tile"
                                            mxEvent={this.props.mxEvent}
@@ -752,7 +752,7 @@ module.exports = withMatrixClient(React.createClass({
                             <a href={permalink} onClick={this.onPermalinkClicked}>
                                 { timestamp }
                             </a>
-                            { this._renderE2EPadlock() }
+                            {/*{ this._renderE2EPadlock() }*/}
                             { thread }
                             <EventTileType ref="tile"
                                            mxEvent={this.props.mxEvent}
