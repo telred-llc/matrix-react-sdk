42ba1554d7ea30f7578143c89f0de8d6d2321e21
Thanh Nguyen <vnscriptkid@gmail.com>
2019-07-25 13:21:08 +0700

    remove startNewChat Button in ChatCreateOrReuseDialog

--- src/components/views/dialogs/ChatCreateOrReuseDialog.js
+++ src/components/views/dialogs/ChatCreateOrReuseDialog.js
@@ -35,9 +35,9 @@ export default class ChatCreateOrReuseDialog extends React.Component {
             tiles: [],
             profile: {
                 displayName: null,
-                avatarUrl: null,
+                avatarUrl: null
             },
-            profileError: null,
+            profileError: null
         };
     }
 
@@ -47,21 +47,25 @@ export default class ChatCreateOrReuseDialog extends React.Component {
         const dmRoomMap = new DMRoomMap(client);
         const dmRooms = dmRoomMap.getDMRoomsForUserId(this.props.userId);
 
-        const RoomTile = sdk.getComponent("rooms.RoomTile");
+        const RoomTile = sdk.getComponent('rooms.RoomTile');
 
         const tiles = [];
         for (const roomId of dmRooms) {
             const room = client.getRoom(roomId);
-            if (room && room.getMyMembership() === "join") {
+            if (room && room.getMyMembership() === 'join') {
                 const member = room.getMember(this.props.userId);
-                if (!member || member.membership !== "join") {
+                if (!member || member.membership !== 'join') {
                     continue;
                 }
 
-                const isInvite = room.getMyMembership() === "invite";
-                const highlight = room.getUnreadNotificationCount('highlight') > 0 || isInvite;
+                const isInvite = room.getMyMembership() === 'invite';
+                const highlight =
+                    room.getUnreadNotificationCount('highlight') > 0 ||
+                    isInvite;
                 tiles.push(
-                    <RoomTile key={room.roomId} room={room}
+                    <RoomTile
+                        key={room.roomId}
+                        room={room}
                         transparent={true}
                         collapsed={false}
                         selected={false}
@@ -69,43 +73,53 @@ export default class ChatCreateOrReuseDialog extends React.Component {
                         highlight={highlight}
                         isInvite={isInvite}
                         onClick={this.onRoomTileClick}
-                    />,
+                    />
                 );
             }
         }
 
         this.setState({
-            tiles: tiles,
+            tiles: tiles
         });
 
         if (tiles.length === 0) {
             this.setState({
-                busyProfile: true,
+                busyProfile: true
             });
-            MatrixClientPeg.get().getProfileInfo(this.props.userId).done((resp) => {
-                const profile = {
-                    displayName: resp.displayname,
-                    avatarUrl: null,
-                };
-                if (resp.avatar_url) {
-                    profile.avatarUrl = MatrixClientPeg.get().mxcUrlToHttp(
-                        resp.avatar_url, 48, 48, "crop",
-                    );
-                }
-                this.setState({
-                    busyProfile: false,
-                    profile: profile,
-                });
-            }, (err) => {
-                console.error(
-                    'Unable to get profile for user ' + this.props.userId + ':',
-                    err,
+            MatrixClientPeg.get()
+                .getProfileInfo(this.props.userId)
+                .done(
+                    resp => {
+                        const profile = {
+                            displayName: resp.displayname,
+                            avatarUrl: null
+                        };
+                        if (resp.avatar_url) {
+                            profile.avatarUrl = MatrixClientPeg.get().mxcUrlToHttp(
+                                resp.avatar_url,
+                                48,
+                                48,
+                                'crop'
+                            );
+                        }
+                        this.setState({
+                            busyProfile: false,
+                            profile: profile
+                        });
+                    },
+                    err => {
+                        console.error(
+                            'Unable to get profile for user ' +
+                                this.props.userId +
+                                ':',
+                            err
+                        );
+                        this.setState({
+                            busyProfile: false,
+                            profileError: err
+                        });
+                    }
                 );
-                this.setState({
-                    busyProfile: false,
-                    profileError: err,
-                });
-            });
         }
     }
 
@@ -125,28 +139,42 @@ export default class ChatCreateOrReuseDialog extends React.Component {
             title = _t('Create a new chat or reuse an existing one');
             const labelClasses = classNames({
                 mx_MemberInfo_createRoom_label: true,
-                mx_RoomTile_name: true,
+                mx_RoomTile_name: true
             });
-            const startNewChat = <AccessibleButton
-                className="mx_MemberInfo_createRoom"
-                onClick={this.props.onNewDMClick}
-            >
-                <div className="mx_RoomTile_avatar">
-                    <img src={require("../../../../res/img/create-big.svg")} width="26" height="26" />
+            const startNewChat = (
+                <AccessibleButton
+                    className='mx_MemberInfo_createRoom'
+                    onClick={this.props.onNewDMClick}
+                >
+                    <div className='mx_RoomTile_avatar'>
+                        <img
+                            src={require('../../../../res/img/create-big.svg')}
+                            width='26'
+                            height='26'
+                        />
+                    </div>
+                    <div className={labelClasses}>
+                        <i>{_t('Start new chat')}</i>
+                    </div>
+                </AccessibleButton>
+            );
+            content = (
+                <div className='mx_Dialog_content' id='mx_Dialog_content'>
+                    {_t(
+                        'You already have existing direct chats with this user:'
+                    )}
+                    <div className='mx_ChatCreateOrReuseDialog_tiles'>
+                        {this.state.tiles}
+                        {/*{ startNewChat }*/}
+                    </div>
                 </div>
-                <div className={labelClasses}><i>{ _t("Start new chat") }</i></div>
-            </AccessibleButton>;
-            content = <div className="mx_Dialog_content" id='mx_Dialog_content'>
-                { _t('You already have existing direct chats with this user:') }
-                <div className="mx_ChatCreateOrReuseDialog_tiles">
-                    { this.state.tiles }
-                    { startNewChat }
-                </div>
-            </div>;
+            );
         } else {
             // Show the avatar, name and a button to confirm that a new chat is requested
             const BaseAvatar = sdk.getComponent('avatars.BaseAvatar');
-            const DialogButtons = sdk.getComponent('views.elements.DialogButtons');
+            const DialogButtons = sdk.getComponent(
+                'views.elements.DialogButtons'
+            );
             const Spinner = sdk.getComponent('elements.Spinner');
             title = _t('Start chatting');
 
@@ -154,41 +182,57 @@ export default class ChatCreateOrReuseDialog extends React.Component {
             if (this.state.busyProfile) {
                 profile = <Spinner />;
             } else if (this.state.profileError) {
-                profile = <div className="error" role="alert">
-                    Unable to load profile information for { this.props.userId }
-                </div>;
-            } else {
-                profile = <div className="mx_ChatCreateOrReuseDialog_profile">
-                    <BaseAvatar
-                        name={this.state.profile.displayName || this.props.userId}
-                        url={this.state.profile.avatarUrl}
-                        width={48} height={48}
-                    />
-                    <div className="mx_ChatCreateOrReuseDialog_profile_name">
-                        { this.state.profile.displayName || this.props.userId }
+                profile = (
+                    <div className='error' role='alert'>
+                        Unable to load profile information for{' '}
+                        {this.props.userId}
                     </div>
-                </div>;
+                );
+            } else {
+                profile = (
+                    <div className='mx_ChatCreateOrReuseDialog_profile'>
+                        <BaseAvatar
+                            name={
+                                this.state.profile.displayName ||
+                                this.props.userId
+                            }
+                            url={this.state.profile.avatarUrl}
+                            width={48}
+                            height={48}
+                        />
+                        <div className='mx_ChatCreateOrReuseDialog_profile_name'>
+                            {this.state.profile.displayName ||
+                                this.props.userId}
+                        </div>
+                    </div>
+                );
             }
-            content = <div>
-                <div className="mx_Dialog_content" id='mx_Dialog_content'>
-                    <p>
-                        { _t('Click on the button below to start chatting!') }
-                    </p>
-                    { profile }
+            content = (
+                <div>
+                    <div className='mx_Dialog_content' id='mx_Dialog_content'>
+                        <p>
+                            {_t('Click on the button below to start chatting!')}
+                        </p>
+                        {profile}
+                    </div>
+                    <DialogButtons
+                        primaryButton={_t('Start Chatting')}
+                        onPrimaryButtonClick={this.props.onNewDMClick}
+                        focus={true}
+                    />
                 </div>
-                <DialogButtons primaryButton={_t('Start Chatting')}
-                    onPrimaryButtonClick={this.props.onNewDMClick} focus={true} />
-            </div>;
+            );
         }
 
         const BaseDialog = sdk.getComponent('views.dialogs.BaseDialog');
         return (
-            <BaseDialog className='mx_ChatCreateOrReuseDialog'
+            <BaseDialog
+                className='mx_ChatCreateOrReuseDialog'
                 onFinished={this.onFinished}
                 title={title}
                 contentId='mx_Dialog_content'
             >
-                { content }
+                {content}
             </BaseDialog>
         );
     }
@@ -199,5 +243,5 @@ ChatCreateOrReuseDialog.propTypes = {
     // Called when clicking outside of the dialog
     onFinished: PropTypes.func.isRequired,
     onNewDMClick: PropTypes.func.isRequired,
-    onExistingRoomSelected: PropTypes.func.isRequired,
+    onExistingRoomSelected: PropTypes.func.isRequired
 };
